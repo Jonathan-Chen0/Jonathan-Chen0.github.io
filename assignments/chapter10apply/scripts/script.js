f5d28c4ee1f9dc2a691db63f481aa4abf8c025c9
Jonathan-Chen0 <jonathan.chen@stu.cvccworks.edu>
2024-10-16 11:32:48 -0400

    Add files via upload

---
+++ assignments/chapter10apply/scripts/script.js
@@ -0,0 +1,11 @@
+/*
+    Student Name: Jonathan Chen
+    File Name: script.js
+    Date: 15 October 2024
+*/
+function content(){
+    var text = document.getElementById("new");
+    text.textContent = "Hi. I am a new paragraph that was created with a JavaScript function.";
+    text.style.color = "#c0145f";
+    text.style.fontSize = "2em";
+}
